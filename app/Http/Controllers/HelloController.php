a02a09544fa7a66553c1bef93bb6db304d8f619e
中岡黎 <reideeplearning@gmail.com>
2019-03-31 00:28:15 +0900

    [fix]コードの修正

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,9 +7,15 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
 
-  public function index(Request $request){
+  public function index(){
     $data = ['msg'=>'これはbladeを使ったサンプルです。'];
     return view('hello.index', $data);
 
+  }
+
+  public function post(Request $request){
+    $msg = $request->msg;
+    $data = ['msg'=>'こんにちわ'. $msg];
+    return view('hello.index', $data);
   }
 }
