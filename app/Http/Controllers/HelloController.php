c0a23a9fdb7e6dfd890bdeb66bc3355d38218bf4
中岡黎 <reideeplearning@gmail.com>
2019-04-15 01:56:22 +0900

    [add]メールの追加ページ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -18,4 +18,23 @@ class HelloController extends Controller
       return view('hello.index', ['items' => $items]);
   }
 
+  public function post(Request $request){
+      $items = DB::select('serect * form  people');
+      return view('hello.index', ['items' => $items]);
+  }
+
+  public function add(Request $request){
+      return view('hello.add');
+  }
+
+  public function create(Request $request){
+      $param = [
+          'name' => $request->name,
+          'mail' => $request->mail,
+          'age' => $request->age,
+      ];
+      DB::select('insert into people (name, mail, age) values(:name, :mail, :age)', $param);
+      return redirect('/hello');
+  }
+
 }
